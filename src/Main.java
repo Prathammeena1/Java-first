51052867ca0100080f82d673b4b78d1dcca1c69e
pratham meena <yashmeena274@gmail.com>
2024-09-16 16:16:34 +0530

    adding 2 numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hey");
+    Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number 1 : ");
+    int num1 = sc.nextInt();
+        System.out.println("Enter the number 2 : ");
+    int num2 = sc.nextInt();
+        System.out.println("The sum is = " + (num1 + num2));
     }
 }
