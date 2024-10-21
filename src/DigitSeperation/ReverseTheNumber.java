6e9fd5a894f5563843c24bff861d0e33f6b14fa4
pratham meena <yashmeena274@gmail.com>
2024-10-21 20:14:03 +0530

    ReverseTheNumber

---
+++ src/DigitSeperation/ReverseTheNumber.java
@@ -0,0 +1,13 @@
+package DigitSeperation;
+
+public class ReverseTheNumber {
+    public static void main(String[] args) {
+        int n = 1234;
+        int rev = 0;
+        while (n != 0){
+            rev = (rev * 10 ) + n%10;
+            n/=10;
+        }
+        System.out.println(rev);
+    }
+}
