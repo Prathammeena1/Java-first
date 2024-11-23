e102050c269f3c7642d8b9cfae780a8402dd4f2a
pratham meena <yashmeena274@gmail.com>
2024-11-23 11:26:11 +0530

    MirrorRightAngleTriangle

---
+++ src/OOPs/Demo.java
@@ -0,0 +1,11 @@
+package OOPs;
+
+public class Demo {
+    public static void main(String[] args) {
+    Student student = new Student("Pratham",21);
+    Student student2 = new Student();
+    System.out.println(student);
+    System.out.println(student2);
+
+    }
+}
