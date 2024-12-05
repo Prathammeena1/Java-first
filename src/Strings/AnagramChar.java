872c2fd18ed5d3e797daf1d9645c8216e08af87b
pratham meena <yashmeena274@gmail.com>
2024-12-05 19:29:49 +0530

    AnagramChar

---
+++ src/Strings/AnagramChar.java
@@ -0,0 +1,25 @@
+package Strings;
+
+import java.util.Arrays;
+
+public class AnagramChar {
+    public static void main(String[] args) {
+        String s = "car";
+        String s1 = "arc";
+
+//        char[] arr1 = s.toCharArray();
+//        char[] arr2 = s1.toCharArray();
+//
+//        Arrays.sort(arr1);
+//        Arrays.sort(arr2);
+//
+//        System.out.println(Arrays.equals(arr1,arr2));
+
+
+//        bitMap concept
+
+//        int arr = 
+
+
+    }
+}
